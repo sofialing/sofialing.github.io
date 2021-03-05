ca2a78a479e1ca9ef99f2ad4af0a27ce81bde4ae
Sofia Ling <sofialing@live.se>
2021-03-05 16:22:44 +0100

    Updated Gatsby config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,34 @@
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
+require("dotenv").config()
 
 module.exports = {
   /* Your site config here */
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-plugin-web-font-loader',
+      options: {
+        typekit: {
+          id: process.env.TYPEKIT_ID,
+        },
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/projects/`,
+      },
+    },
+  ],
+  siteMetadata: {
+    title: 'Sofia Ling',
+    subtitle: 'Hello, I\'m a Web Developer and Designer',
+    description: 'Currently studying at Medieinstitutet in Malmö, Sweden.',
+    linkedin_url: 'https://www.linkedin.com/in/sofia-ling-118a19a8/',
+    github_url: 'https://github.com/sofialing',
+    dribbble_url: 'https://dribbble.com/sofialing',
+  },
 }
