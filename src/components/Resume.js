a437b1adf354bcad0284ca6cfa4dca9a78057e86
Sofia Ling <sofialing@live.se>
2021-03-06 15:24:01 +0100

    Updated data and added photos.

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,51 +4,6 @@ import { container, columns, column } from '../styles/container.module.scss'
 import { blue } from '../styles/buttons.module.scss'
 import Timeline from './Timeline';
 
-const work = [
-    {
-        position: 'Graphic Designer',
-        company: 'Kristianstadsbladet, Kristianstad',
-        time: 'March 2016 - present'
-    },
-    {
-        position: 'Final Art',
-        company: 'Mustasch Reklambyrå, Karlskrona',
-        time: 'February 2015 - December 2015'
-    },
-    {
-        position: 'Graphic Designer',
-        company: 'Viron Media, Karlshamn',
-        time: 'September 2014 - November 2014'
-    },
-    {
-        position: 'Intern',
-        company: 'Bold Design, Stockholm',
-        time: 'November 2012 - January 2013'
-    }
-];
-const education = [
-    {
-        position: 'Front End Developer',
-        company: 'Medieinstitutet, Malmö',
-        time: 'September 2019 - present'
-    },
-    {
-        position: 'Web Programming & Databases',
-        company: 'Blekinge Tekniska Högskola, Karlskrona',
-        time: 'September 2018 - June 2019'
-    },
-    {
-        position: 'Advertising, Graphic Design & Visual Communication',
-        company: 'Linnéuniversitetet, Kalmar',
-        time: 'September 2010 - June 2013'
-    },
-    {
-        position: 'IT-Media',
-        company: 'John Bauergymnasiet, Västerås',
-        time: 'September 2007 - June 2010'
-    }
-]
-
 export default function Resume() {
     const data = useStaticQuery(graphql`
         {
@@ -74,7 +29,6 @@ export default function Resume() {
         }
     `)
     const { linkedin_url } = data.site.siteMetadata
-    console.log('resume data', data);
     return (
         <section id="resume">
             <div className={container}>
