f25ea815a0d93649847411dffcc354e89e50cce6
Sofia Ling <sofialing@live.se>
2021-03-07 14:04:32 +0100

    Added skills.

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,7 +1,26 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
 import { container, columns, column } from '../styles/container.module.scss'
+import { badges } from '../styles/badges.module.scss'
 
 export default function Skills() {
+    const data = useStaticQuery(graphql`
+        {
+            skillsJson {
+                id
+                design {
+                  alt
+                  src
+                }
+                development {
+                  alt
+                  src
+                }
+            }
+        }
+    `)
+    const { design, development } = data.skillsJson;
+    console.log(design, development);
     return (
         <section id="skills">
             <div className={container}>
@@ -10,14 +29,17 @@ export default function Skills() {
                     <div className={column}>
                         <h3>Web Development</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel dui et ligula sodales sodales.</p>
+                        <ul className={badges}>
+                            {development.map((item, i) => <li key={i}><img src={item.src} alt={item.alt} /></li>)}
+                        </ul>
+
                     </div>
                     <div className={column}>
                         <h3>Design</h3>
                         <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel dui et ligula sodales sodales.</p>
-                    </div>
-                    <div className={column}>
-                        <h3>WordPress</h3>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce vel dui et ligula sodales sodales.</p>
+                        <ul className={badges}>
+                            {design.map((item, i) => <li key={i}><img src={item.src} alt={item.alt} /></li>)}
+                        </ul>
                     </div>
                 </div>
             </div>
